cf5bb38a6d2dff450c27b73e49077509b592b709
Jader Santiago Nieves <jsnieves39@ucatolica.edu.co>
2025-10-30 05:06:25 -0500

    Creacion de carpeta unicamente con los modelos

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Paquete de modelos del sistema universitario
+"""
+from .estudiante import Estudiante
+from .curso import Curso
+from .matricula import Matricula
+
+__all__ = ["Estudiante", "Curso", "Matricula"]
